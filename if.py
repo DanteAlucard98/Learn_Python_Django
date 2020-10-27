aef9368fd1c856b54176d66aa518337fab88afaa
Daniel Perez <dantealucard98.dp@gmail.com>
2020-10-27 12:21:14 -0600

    Aprender If/Elsif

---
+++ if.py
@@ -0,0 +1,11 @@
+"""Clase de If/else/Elif."""
+
+my_number = int(input('Ingresa un numero entero: '))
+
+if 5 > my_number:
+    #f es para meter el formato de string para meter el valor de una variable
+    print(f'5 es mayor que {my_number}')
+elif 5 == my_number:
+    print(f'{my_number} es igual 5')
+else:
+    print(f'5 es menor a {my_numbe}')
